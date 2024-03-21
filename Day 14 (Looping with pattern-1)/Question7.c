2fb727ff935697b5e15eba99a1f87440cc590d2a
Prince9510 <satasiyaprince68@gmail.com>
2024-03-21 15:41:13 +0530

    Day 14 (Looping with pattern-1 )

---
+++ Day 14 (Looping with pattern-1)/Question7.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+
+main(){
+
+
+   for (int i = 65; i <= 69; i++)
+   {
+        for (int j = i; j >=65; j--)
+        {
+            printf("%c",j);
+        }
+        printf("\n");    
+   }
+   
+    
+}
